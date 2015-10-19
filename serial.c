56b50ed08127c551fd0c65d98619639716aa3449
Daniel Garcia-Briseno <dg.briseno@gmail.com>
2015-10-18 19:57:40 -0700

    Uart now working correctly

--- serial.c
+++ serial.c
@@ -1,16 +1,16 @@
 #define F_CPU 1000000
-#define BAUD 12                        // Baud in bits per second
+#define BAUD 4800
+#define BAUDRATE ((F_CPU/(BAUD*16UL))-1)                        // Baud in bits per second
 
+#include <stdio.h>
 #include <avr/io.h>
 
 void uart_init(void) 
 {
-    UBRR0H = 0; // Put high 8 bits in H register
-    UBRR0L = 0;        // Put low 8 bits in L register
     UCSR0B |= (1 << TXEN0)|(1<<RXEN0);
-    UCSR0C = 0;
-    UBRR0H = (BAUD >> 8); // Put high 8 bits in H register
-    UBRR0L = BAUD;        // Put low 8 bits in L register
+    UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+    UBRR0H = BAUDRATE >> 8; // Put high 8 bits in H register
+    UBRR0L = BAUDRATE;        // Put low 8 bits in L register
 }
 
 
